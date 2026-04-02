a239ffe03531fd8259d77fed3ffe0584c8fe281c
WhoisMari <mfilippozzi0@gmail.com>
2026-04-02 18:02:42 +0100

    Use CompressedStaticFilesStorage to preserve React hashed filenames

--- backend/settings.py
+++ backend/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / 'build/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
